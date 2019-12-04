2163da144c0487a81127aa4761a97a3fc8114dc3
Andrew Rowson <github@growse.com>
2019-12-04 20:40:14 +0000

    Day 1 complete!

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -2,8 +2,9 @@ package com.growse.adventofcode
 
 import kotlin.math.floor
 
-fun main(args: Array<String>) {
-    print(Day1().sumFuelRequiredForMasses())
+fun main() {
+    println(Day1().sumFuelRequiredForMasses())
+    println(Day1().sumFuelRequiredForMassesAddingFuelForTheFuelToo())
 }
 
 class Day1 {
@@ -19,7 +20,29 @@ class Day1 {
             .orElseThrow()
     }
 
+    fun sumFuelRequiredForMassesAddingFuelForTheFuelToo(): Int {
+        return this::class
+            .java
+            .getResourceAsStream("/day1.input.txt")
+            .bufferedReader()
+            .lines()
+            .map { line -> line.toInt() }
+            .map { mass -> calculateFuelRequiredConsideringFuelAsWell(mass) }
+            .reduce { current, value -> current + value }
+            .orElseThrow()
+    }
+
     fun calculateFuelRequired(inputMass: Int): Int {
-        return (floor((inputMass / 3).toDouble())).toInt() - 2
+        return maxOf((floor((inputMass / 3).toDouble())).toInt() - 2, 0)
+    }
+
+    fun calculateFuelRequiredConsideringFuelAsWell(inputMass: Int): Int {
+        var totalFuel = 0
+        var lastMass = inputMass
+        while (lastMass > 0) {
+            lastMass = calculateFuelRequired(lastMass)
+            totalFuel += lastMass
+        }
+        return totalFuel
     }
 }
--- src/test/kotlin/Day1.kt
+++ src/test/kotlin/Day1.kt
@@ -6,10 +6,17 @@ import org.junit.jupiter.params.provider.CsvSource
 
 
 class TestDay1 {
-    @ParameterizedTest(name = "For mass {0}, thrust is {1}")
+    @ParameterizedTest(name = "For mass {0}, fuel is {1}")
     @CsvSource("12,2", "14,2", "1969,654", "100756,33583")
     fun testFuelIsCorrectForGivenMass(inputMass: Int, expectedFuel: Int) {
         val fuelRequired = Day1().calculateFuelRequired(inputMass)
+        assertEquals(expectedFuel, fuelRequired)
+    }
+
+    @ParameterizedTest(name = "For mass {0}, fuel is {1}")
+    @CsvSource("12,2", "1969,966", "100756,50346")
+    fun testFuelIsCorrectForGivenMassAndFuel(inputMass: Int, expectedFuel: Int) {
+        val fuelRequired = Day1().calculateFuelRequiredConsideringFuelAsWell(inputMass)
         assertEquals(expectedFuel, fuelRequired)
     }
 }
