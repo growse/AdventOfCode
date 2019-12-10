2fbf25b716f02609c9d80d8ce7d2a10d4c424043
Andrew Rowson <github@growse.com>
2019-12-10 23:15:18 +0000

    Day 6 complete!

--- src/main/kotlin/com/growse/adventofcode/Day6.kt
+++ src/main/kotlin/com/growse/adventofcode/Day6.kt
@@ -1,11 +1,17 @@
 package com.growse.adventofcode.com.growse.adventofcode
 
 fun main() {
-    println(Day6().getOrbitChecksumForResource("/day6.input.txt"))
+    println(Day6().calculateOrbitChecksumForResource("/day6.input.txt"))
+    println(Day6().calculateOrbitTransferFromResource("/day6.input.txt"))
 }
 
 class Day6 {
-    fun getOrbitChecksum(input: List<String>): Int {
+    fun calculateOrbitChecksum(input: List<String>): Int {
+        val graph: MutableMap<String, String?> = buildGraph(input)
+        return graph.keys.sumBy { sumForNode(it, graph) }
+    }
+
+    private fun buildGraph(input: List<String>): MutableMap<String, String?> {
         val graph: MutableMap<String, String?> = mutableMapOf()
         input.forEach {
             val nodeNames = it.split(")", limit = 2)
@@ -14,8 +20,7 @@ class Day6 {
             graph[nodeNames[1]] = nodeNames[0]
 
         }
-        return graph.keys.sumBy { sumForNode(it, graph) }
-
+        return graph
     }
 
     private fun sumForNode(key: String, graph: Map<String, String?>): Int {
@@ -26,7 +31,7 @@ class Day6 {
         }
     }
 
-    fun getInputProgram(resourceName: String): List<String> =
+    private fun getInputProgram(resourceName: String): List<String> =
         this::class
             .java
             .getResourceAsStream(resourceName)
@@ -34,8 +39,31 @@ class Day6 {
             .use { it.readText() }
             .split("\n")
 
-    fun getOrbitChecksumForResource(s: String): Int {
-        return getOrbitChecksum(getInputProgram(s))
+    fun calculateOrbitChecksumForResource(s: String): Int {
+        return calculateOrbitChecksum(getInputProgram(s))
+    }
 
+    fun calculateOrbitTransfer(input: List<String>, from: String, to: String): Int {
+        val graph = buildGraph(input)
+
+        val fromParentList = parents(graph, from)
+        val toParentList = parents(graph, to)
+        val commonParents = fromParentList.asReversed().intersect(toParentList.asReversed())
+        return fromParentList.indexOf(commonParents.last()) + toParentList.indexOf(commonParents.last())
+
+    }
+
+    private fun parents(graph: Map<String, String?>, node: String): MutableList<String> {
+        val ret = mutableListOf<String>()
+        var current = node
+        while (graph[current] != null) {
+            ret.add(graph[current]!!)
+            current = graph[current]!!
+        }
+        return ret
+    }
+
+    fun calculateOrbitTransferFromResource(s: String): Int {
+        return calculateOrbitTransfer(getInputProgram(s), "YOU", "SAN")
     }
 }
--- src/test/kotlin/TestDay6.kt
+++ src/test/kotlin/TestDay6.kt
@@ -1,5 +1,6 @@
 import com.growse.adventofcode.com.growse.adventofcode.Day6
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -34,6 +35,27 @@ class TestDay6 {
     @ParameterizedTest
     @MethodSource("orbitTestCases")
     fun orbitChecksumIsCorrect(input: List<String>, expectedOutput: Int) {
-        assertEquals(expectedOutput, Day6().getOrbitChecksum(input))
+        assertEquals(expectedOutput, Day6().calculateOrbitChecksum(input))
+    }
+
+    @Test
+    fun orbitTransferIsCorrect() {
+        val input = listOf(
+            "COM)B",
+            "B)C",
+            "C)D",
+            "D)E",
+            "E)F",
+            "B)G",
+            "G)H",
+            "D)I",
+            "E)J",
+            "J)K",
+            "K)L",
+            "K)YOU",
+            "I)SAN"
+        )
+        val expectedOutput = 4
+        assertEquals(expectedOutput, Day6().calculateOrbitTransfer(input, "YOU", "SAN"))
     }
 }
